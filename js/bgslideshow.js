ddee933471d12d8af918b7423c650a4932c0d54e
TETRAPOD211 <yoshiki3260@gmail.com>
2024-10-24 14:33:04 +0900

    initial comit
    ポートフォリオ１

---
+++ js/bgslideshow.js
@@ -0,0 +1,14 @@
+// bgswitcher-script
+/* <script src="../js/jquery.bgswitcher.js">
+</script> */
+
+
+jQuery(function($) {
+ $('.bgimg-slider').bgSwitcher({
+ images: ['../image/bgi/Top_bg.jpg','../image/bgi/Top_slaid.jpg','../image/bgi/Top_slaid2.jpg'],
+ effect: "drop",
+ easing: "linear"
+ });
+});
+
+// bgswitcher-script-end
